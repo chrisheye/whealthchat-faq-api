042f4b0d91761e8e16a36a41834ca477c19cbe94
chrisheye <chris@whealthcareplan.com>
2025-07-19 09:48:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -90,8 +90,14 @@ async def get_faq_answer(request: Request):
 
     # 1. Try exact match
     exact_results = collection.query.fetch_objects(
-        filters=Filter.by_property("questionExact").equal(q)
-        .and_filter(Filter.by_property("user").equal(user)),
+        filters=Filter.by_combination(
+        operator="And",
+        filters=[
+        Filter.by_property("questionExact").equal(q),
+        Filter.by_property("user").equal(user)
+    ]
+),
+
         limit=1
     )
     if exact_results.objects:
