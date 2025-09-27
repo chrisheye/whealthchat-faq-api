d8e9c61ef39b06793937176bd3f83927e1d9f8db
chrisheye <chris@whealthcareplan.com>
2025-09-27 15:43:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,8 @@ async def get_faq(request: Request):
         user_filt = Filter.by_property("user").equal("both") | Filter.by_property("user").equal(requested_user)
         combined_filt = and_filters(user_filt, tenant_filt)
 
-        filter = Filter.by_property("question").equal(raw_q.strip()) & combined_filt
+        filter = Filter.by_property("question").equal(raw_q.strip())
+
         print("🔎 exact-match allowed_sources:", allowed)
 
         exact_res = collection.query.fetch_objects(
